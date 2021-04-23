d9fe2a18806b24b7da004cf58f0b8341f34050f4
ssmeds <stina_smeds@hotmail.com>
2021-04-23 21:40:40 +0200

    removed everything from index.html and put it in get / for admin instead

--- app.js
+++ app.js
@@ -28,7 +28,9 @@ app.use(express.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+  index: false
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/admin.js
+++ routes/admin.js
@@ -7,17 +7,29 @@ router.use(cors());
 
 /* GET users listing. */
 router.get('/', (req, res) => {
-  let adminInputName = req.body.userName;
-  let adminInputPass = req.body.userPass;
 
-  if (adminInputName === "admin" && adminInputPass === "admin") {
-    console.log("get / answer");
-    res.send(adminInputName);
-    res.redirect('https://stinas-newsletter.herokuapp.com/admin/users')
-  } else {
-    console.log("redirect from get /");
-    res.redirect('back')
-  }
+
+  let checkForAdmin = `<div>
+  <h1>Inloggning för administratörer</h1>
+  <form action="https://stinas-newsletter.herokuapp.com/admin" method="post">
+    <input type="text" name="userName" id="userName" placeholder="Användarnamn">
+    <input type="password" name="userPass" id="password" placeholder="Lösenord">
+    <button type="submit">Logga in</button></form>
+</div>`
+
+  // let adminInputName = req.body.userName;
+  //   let adminInputPass = req.body.userPass;
+
+
+  // if (adminInputName === "admin" && adminInputPass === "admin") {
+  //   console.log("get / answer");
+  //   res.send(adminInputName);
+  //   res.redirect('https://stinas-newsletter.herokuapp.com/admin/users')
+  // } else {
+  //   console.log("redirect from get /");
+  //   res.redirect('back')
+  // }
+  res.send(checkForAdmin);
 })
 
 router.post('/', function (req, res, next) {
@@ -27,34 +39,50 @@ router.post('/', function (req, res, next) {
 
   if (adminInputName === "admin" && adminInputPass === "admin") {
     console.log("post / answer");
-    res.send(adminInputName);
-    res.redirect('https://stinas-newsletter.herokuapp.com/admin/users')
+    // res.send(adminInputName);
+    // res.redirect('https://stinas-newsletter.herokuapp.com/admin/users')
+    req.app.locals.db.collection("users").find().toArray()
+      .then(results => {
+        console.log(results);
+        let showUsers = `<div><h2>Alla användare</h2>`
+
+        // <script>
+        //   if(!localStorage.getItem("adminloggedin")) {
+        // window.location.replace("https://stinas-newsletter.herokuapp.com/admin");
+        //   }</script>
+
+        for (user in results) {
+          showUsers += `<div>Användarnamn: ${results[user].userName} - Email: ${results[user].userEmail} - Lösenord: ${results[user].userPass} - Id: ${results[user].userId} - Nyhetsbrev: ${results[user].newsletter}</div>`
+        }
+        showUsers += `</div>`
+        res.send(showUsers);
+      })
   } else {
     console.log("redirect from post /");
     res.redirect('back')
   }
 });
 
-router.get('/users', (req, res) => {
+// router.get('/users', (req, res) => {
 
-  req.app.locals.db.collection("users").find().toArray()
-    .then(results => {
-      console.log(results);
-      let showUsers =
-        `
-        // <script>
-        //   if(!localStorage.getItem("adminloggedin")) {
-        // window.location.replace("https://stinas-newsletter.herokuapp.com/admin");
-        //   }</script>
-          
-        <div><h2>Alla användare</h2>`
+// req.app.locals.db.collection("users").find().toArray()
+//   .then(results => {
+//     console.log(results);
+//     let showUsers =
+//       `
+//       // <script>
+//       //   if(!localStorage.getItem("adminloggedin")) {
+//       // window.location.replace("https://stinas-newsletter.herokuapp.com/admin");
+//       //   }</script>
 
-      for (user in results) {
-        showUsers += `<div>Användarnamn: ${results[user].userName} - Email: ${results[user].userEmail} - Lösenord: ${results[user].userPass} - Id: ${results[user].userId} - Nyhetsbrev: ${results[user].newsletter}</div>`
-      }
-      showUsers += `</div>`
-      res.send(showUsers);
-    })
-});
+//       <div><h2>Alla användare</h2>`
+
+//     for (user in results) {
+//       showUsers += `<div>Användarnamn: ${results[user].userName} - Email: ${results[user].userEmail} - Lösenord: ${results[user].userPass} - Id: ${results[user].userId} - Nyhetsbrev: ${results[user].newsletter}</div>`
+//     }
+//     showUsers += `</div>`
+//     res.send(showUsers);
+//   })
+// });
 
 module.exports = router;
