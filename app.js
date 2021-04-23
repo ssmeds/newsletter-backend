901cbd19c746d197d8e98fdb55695dc1550d4412
ssmeds <stina_smeds@hotmail.com>
2021-04-23 21:46:01 +0200

    changed app.use / from users to admin

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public'), {
   index: false
 }));
 
-app.use('/', usersRouter);
+app.use('/', adminRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
 
